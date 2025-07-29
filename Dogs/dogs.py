c2f93bdd8100a19b6dbe79ff61583f1b9244fece
prostoTanya <tyakovleva2014@gmail.com>
2025-07-29 20:07:56 +0300

    Создана функция загрузки изображения в метку

--- Dogs/dogs.py
+++ Dogs/dogs.py
@@ -1,7 +1,28 @@
 from tkinter import *
+from  tkinter import messagebox as mb
 import  requests
 from PIL import Image, ImageTk
 from io import BytesIO
+
+from bottle import response
+
+
+def show_img():
+    img_url = get_dog_img()
+    if img_url:
+        try:
+            response = requests.get(img_url, stream=True)
+            response.raise_for_status()
+            img_data = BytesIO(response.content)
+            img = Image.open(img_data)
+            img.thumbnail((300, 300))
+            lab.config(image=img)
+            lab.img = img
+        except Exception as e:
+            mb.showerror('Ошибка', f'Возникла ошибка {e}')
+
+
+
 
 window = Tk()
 window.title('Картинки с собачками')
